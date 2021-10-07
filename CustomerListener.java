f505ba5331df9627aca077a6d4cb8a4c0008542c
ying <2102287858@qq.com>
2021-10-08 00:37:58 +0800

    third

---
+++ CustomerListener.java
@@ -0,0 +1,17 @@
+package homework;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class CustomerListener implements ActionListener{
+	public void actionPerformed(ActionEvent e)
+	{
+		String z=e.getActionCommand();
+		if(z=="1111111111111111")
+		{
+			System.out.println("0");
+		}
+	}
+	
+	
+
+}
